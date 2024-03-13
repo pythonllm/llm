09fd0b11df1abca3da9007dadd0ac98151b85569
pythonllm <161817327+pythonllm@users.noreply.github.com>
2024-03-13 16:12:47 +0200

    Update connection.py

--- connection.py
+++ connection.py
@@ -1,22 +1,24 @@
 from llama_cpp import Llama
 
-# Set gpu_layers to the number of layers to offload to GPU. Set to 0 if no GPU acceleration is available on your system.
 llm = Llama(
-    model_path="./solar-10.7b-instruct-v1.0-uncensored.Q4_K_M.gguf",  # Download the model file first
-    n_ctx=4096,  # The max sequence length to use - note that longer sequence lengths require much more resources
-    n_threads=8,  # The number of CPU threads to use, tailor to your system and the resulting performance
-    n_gpu_layers=35  # The number of layers to offload to GPU, if you have GPU acceleration available
+    model_path="./solar-10.7b-instruct-v1.0-uncensored.Q8_0.gguf",
+    n_gpu_layers=-1,  # Utilize GPU for all layers
+    n_batch=512,  # Number of tokens to process in parallel
+    n_threads=24,  # Utilize all vCPUs for threading
+    verbose=True,  # Enable verbose logging
+    max_tokens=512,
 )
 
-# Simple inference example
+
+prompt_temp = "You are Paolina, a very attractive 20-year-old, flirty, and playful adult model female obsessed with sex. Form detailed answers in a brutal sexual manner"
+
+
 output = llm(
-    "{prompt}",  # Prompt
-    max_tokens=512,  # Generate up to 512 tokens
-    stop=["</s>"],  # Example stop token - not necessarily correct for this specific model! Please check before using.
-    echo=True  # Whether to echo the prompt
+    prompt_temp,
+    max_tokens=32,  # Generate up to 512 tokens
+    stop=["</s>"],  # Assuming "</s>" is a valid stop token for your model
+    echo=True  # Echo the prompt in the output
 )
 
-# Chat Completion API
 
-llm = Llama(model_path="./solar-10.7b-instruct-v1.0-uncensored.Q4_K_M.gguf",
-            chat_format="llama-2")  # Set chat_format according to the model you are using
+
